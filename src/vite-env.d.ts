3505fa4240461e0a627a6ed5fea959c37aa32d0b
faizjie9 <ilmuwankecil@gmail.com>
2024-11-18 11:00:16 +0700

    fix: add React type declarations and move type packages to dependencies

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="react/jsx-runtime" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
